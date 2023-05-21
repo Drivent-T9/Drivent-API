a3b352688991e4e2a812d73e1835185519b453f0
Guilherme Tchornei <guilhermetchornei@gmail.com>
2023-05-21 13:31:34 -0300

    refact: findHotels return freeSlots & typeRoom

--- src/repositories/hotel-repository/index.ts
+++ src/repositories/hotel-repository/index.ts
@@ -2,7 +2,42 @@ import { Hotel, Room } from '@prisma/client';
 import { prisma } from '@/config';
 
 async function findHotels() {
-  return prisma.hotel.findMany();
+  const hotels = await prisma.hotel.findMany({
+    include: {
+      Rooms: {
+        include: {
+          Booking: true,
+        },
+      },
+    },
+  });
+
+  return hotels.map((h) => {
+    const freeSlots = h.Rooms.reduce((accumulator, currentValue) => {
+      return accumulator + currentValue.capacity - currentValue.Booking.length;
+    }, 0);
+    const typeRooms = { single: false, double: false, triple: false };
+    typeRooms.single = h.Rooms.some((r) => r.capacity === 1);
+    typeRooms.double = h.Rooms.some((r) => r.capacity === 2);
+    typeRooms.triple = h.Rooms.some((r) => r.capacity === 3);
+
+    return {
+      id: h.id,
+      image: h.image,
+      name: h.name,
+      freeSlots,
+      typeRooms,
+    } as GetHotel;
+  });
+}
+
+export interface GetHotel extends Pick<Hotel, 'id' | 'name' | 'image'> {
+  freeSlots: number;
+  typeRooms: {
+    single: boolean;
+    double: boolean;
+    triple: boolean;
+  };
 }
 
 async function findRoomsByHotelId(hotelId: number): Promise<Hotel & { Rooms: GetRoomsBooking[] }> {
