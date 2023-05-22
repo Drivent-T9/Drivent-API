decbeea9a1ceaf4a15b1423c697872e50922fba0
Nathalya Ohana Pires Ferreira <nathyohana@gmail.com>
2023-05-22 16:11:38 -0400

    feat: implemented signin with github

--- src/routers/authentication-router.ts
+++ src/routers/authentication-router.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { singInPost } from '@/controllers';
+import { signInWithGitHub, singInPost } from '@/controllers';
 import { validateBody } from '@/middlewares';
 import { signInSchema } from '@/schemas';
 
 const authenticationRouter = Router();
 
 authenticationRouter.post('/sign-in', validateBody(signInSchema), singInPost);
+authenticationRouter.post('/sign-in/github', signInWithGitHub);
 
 export { authenticationRouter };
